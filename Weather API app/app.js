f406e951d8230454b129b7cc2c721fee7d66f378
Matiss1337 <84022755+Matiss1337@users.noreply.github.com>
2022-08-16 19:17:01 +0200

    Add files via upload
    node.js ejs. express 2021

---
+++ Weather API app/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+app.use(express.urlencoded({ extendet: true}));
+const fetch = require('node-fetch');
+
+
+app.use(express.static(__dirname + '/'));
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '/views'))
+
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/', (req, res) => {
+    const city = req.body.city;
+    const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=3f5c7f4669bdbc486c9ff65870f65aee`;
+fetch(urlApi)
+.then(res => res.json())
+.then(data => console.log(data));
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
